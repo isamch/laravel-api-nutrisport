9a85f561051cb3358a622042597485f29793cca8
isam chajia <isamchajiabeat@gmail.com>
2026-02-17 18:27:42 +0000

    configure API to return JSON instead of redirect on unauthenticated

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectGuestsTo(fn () => response()->json(['message' => 'Unauthenticated'], 401));
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
