2970e056322b0bbff3351a1f086bfd81212c75f0
isam chajia <isamchajiabeat@gmail.com>
2026-02-17 21:08:41 +0000

    feat: add localhost default site (France) for development mode

--- src/app/Http/Middleware/DetectSite.php
+++ src/app/Http/Middleware/DetectSite.php
@@ -11,10 +11,10 @@ class DetectSite
     public function handle(Request $request, Closure $next): Response
     {
         $host = $request->getHost();
-        
+
         // Extract domain extension (.fr, .it, .be)
         $extension = substr($host, strrpos($host, '.'));
-        
+
         // Cache site map for 24 hours
         $siteMap = \Cache::remember('site_map', 86400, function () {
             return \DB::table('sites')
@@ -22,12 +22,21 @@ class DetectSite
                 ->mapWithKeys(fn($id, $code) => ['.' . strtolower(substr($code, -2)) => $id])
                 ->toArray();
         });
-        
-        // Set site_id if not already provided and domain matches
-        if (!$request->has('site_id') && isset($siteMap[$extension])) {
-            $request->merge(['site_id' => $siteMap[$extension]]);
+
+        // Set site_id if not already provided
+        if (!$request->has('site_id')) {
+            if (isset($siteMap[$extension])) {
+                // Production: detect from domain
+                $request->merge(['site_id' => $siteMap[$extension]]);
+            } elseif (in_array($host, ['localhost', '127.0.0.1'])) {
+                // Development: default to France
+                $defaultSiteId = \Cache::remember('default_site_id', 86400, function () {
+                    return \DB::table('sites')->where('country_code', 'FR')->value('id');
+                });
+                $request->merge(['site_id' => $defaultSiteId]);
+            }
         }
-        
+
         return $next($request);
     }
 }
